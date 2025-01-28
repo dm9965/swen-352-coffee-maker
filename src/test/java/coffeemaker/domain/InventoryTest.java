e409d4f9a609949933f7c3bffd9fd42699bc1ac8
Jarred Reepmeyer <jsr5569@rit.edu>
2025-01-28 15:31:06 -0500

    Generated test class for Inventory class.

---
+++ src/test/java/coffeemaker/domain/InventoryTest.java
@@ -0,0 +1,67 @@
+package coffeemaker.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class InventoryTest {
+
+    @Test
+    void testGetCoffee() {
+    }
+
+    @Test
+    void testSetCoffee() {
+    }
+
+    @Test
+    void testAddCoffee() {
+    }
+
+    @Test
+    void testGetMilk() {
+    }
+
+    @Test
+    void testSetMilk() {
+    }
+
+    @Test
+    void testAddMilk() {
+    }
+
+    @Test
+    void testGetSugar() {
+    }
+
+    @Test
+    void testSetSugar() {
+    }
+
+    @Test
+    void testAddSugar() {
+    }
+
+    @Test
+    void testGetChocolate() {
+    }
+
+    @Test
+    void testSetChocolate() {
+    }
+
+    @Test
+    void testAddChocolate() {
+    }
+
+    @Test
+    void testEnoughIngredients() {
+    }
+
+    @Test
+    void testUseIngredients() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
