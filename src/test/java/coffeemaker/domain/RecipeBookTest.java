297dcc70c19609f41d28c4ab37ce4fb6f0dd34b6
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2025-01-28 15:05:25 -0500

    auto gen recipe and recipeBook test classes

---
+++ src/test/java/coffeemaker/domain/RecipeBookTest.java
@@ -0,0 +1,24 @@
+package coffeemaker.domain;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RecipeBookTest {
+
+    @Test
+    void getRecipes() {
+    }
+
+    @Test
+    void addRecipe() {
+    }
+
+    @Test
+    void deleteRecipe() {
+    }
+
+    @Test
+    void replaceRecipe() {
+    }
+}
