dd9e8559b1615e6055372876e5722aa282a86b01
mpb8984 <71938683+mpb8984@users.noreply.github.com>
2025-01-31 13:11:14 -0500

    99 coverage

--- src/test/java/coffeemaker/domain/RecipeTest.java
+++ src/test/java/coffeemaker/domain/RecipeTest.java
@@ -1,5 +1,6 @@
 package coffeemaker.domain;
 
+import coffeemaker.exceptions.RecipeException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -26,6 +27,15 @@ class RecipeTest {
     }
 
     @Test
+    void setNameNull()
+    {
+        Recipe newRecipe = new Recipe();
+        newRecipe.setName("Lavazza");
+        newRecipe.setName(null);
+        assertEquals(newRecipe.getName(), "Lavazza");
+    }
+
+    @Test
     void getPrice() {
         assertEquals(recipe.getPrice(), 0);
     }
@@ -36,6 +46,20 @@ class RecipeTest {
         recipe.setPrice(price);
         assertNotEquals(recipe.getPrice(), 0);
         assertEquals(recipe.getPrice(), Integer.parseInt(price));
+    }
+
+    @Test
+    void setPriceException()
+    {
+        String price = "fail9";
+        assertThrows(RecipeException.class, () -> {recipe.setPrice(price);});
+    }
+
+    @Test
+    void setPriceNegative()
+    {
+        String price = "-1";
+        assertThrows(RecipeException.class, () -> {recipe.setPrice(price);});
     }
 
     @Test
@@ -52,6 +76,20 @@ class RecipeTest {
     }
 
     @Test
+    void setAmtCoffeeException()
+    {
+        String coffee = "fail9";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtCoffee(coffee);});
+    }
+
+    @Test
+    void setAmtCoffeeNegative()
+    {
+        String coffee = "-1";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtCoffee(coffee);});
+    }
+
+    @Test
     void getAmtMilk() {
         assertEquals(recipe.getAmtMilk(), 0);
     }
@@ -62,6 +100,20 @@ class RecipeTest {
         recipe.setAmtMilk(milk);
         assertNotEquals(recipe.getAmtMilk(), 0);
         assertEquals(recipe.getAmtMilk(), Integer.parseInt(milk));
+    }
+
+    @Test
+    void setAmtMilkException()
+    {
+        String milk = "fail9";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtMilk(milk);});
+    }
+
+    @Test
+    void setAmtMilkNegative()
+    {
+        String milk = "-1";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtMilk(milk);});
     }
 
     @Test
@@ -78,6 +130,20 @@ class RecipeTest {
     }
 
     @Test
+    void setAmtSugarException()
+    {
+        String sugar = "fail9";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtSugar(sugar);});
+    }
+
+    @Test
+    void setAmtSugarNegative()
+    {
+        String sugar = "-1";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtSugar(sugar);});
+    }
+
+    @Test
     void getAmtChocolate() {
         assertEquals(recipe.getAmtChocolate(), 0);
     }
@@ -91,6 +157,20 @@ class RecipeTest {
     }
 
     @Test
+    void setAmtChocolateException()
+    {
+        String chocolate = "fail9";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtChocolate(chocolate);});
+    }
+
+    @Test
+    void setAmtChocolateNegative()
+    {
+        String chocolate = "-1";
+        assertThrows(RecipeException.class, () -> {recipe.setAmtChocolate(chocolate);});
+    }
+
+    @Test
     void testToString() {
         recipe.setName("Lavazza");
         String expected = "Recipe{Lavazza}";
@@ -101,15 +181,19 @@ class RecipeTest {
     }
 
     @Test
-    void testHashCode() {
+    void testHashCodeEqual() {
         Recipe recipe1 = new Recipe();
-        Recipe recipe2 = new Recipe();
-
         recipe.setName("Latte");
         recipe1.setName("Latte");
-        recipe2.setName("Espresso");
-
         assertEquals(recipe.hashCode(), recipe1.hashCode());
+    }
+
+    @Test
+    void testHashCodeNotEqual()
+    {
+        Recipe recipe2 = new Recipe();
+        recipe.setName("Latte");
+        recipe2.setName("Espresso");
         assertNotEquals(recipe.hashCode(), recipe2.hashCode());
     }
 
@@ -131,8 +215,20 @@ class RecipeTest {
         sameRecipe.setAmtChocolate("4");
         sameRecipe.setAmtCoffee("2");
 
-        Recipe differentRecipe = new Recipe();
+        assertTrue(recipe.equals(sameRecipe));
+    }
 
+    @Test
+    void testNotEquals()
+    {
+        recipe.setName("Lavazza");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        Recipe differentRecipe = new Recipe();
         differentRecipe.setName("Starbucks");
         differentRecipe.setAmtSugar("2");
         differentRecipe.setPrice("1");
@@ -140,13 +236,11 @@ class RecipeTest {
         differentRecipe.setAmtChocolate("4");
         differentRecipe.setAmtCoffee("3");
 
-        assertTrue(recipe.equals(sameRecipe));
         assertFalse(recipe.equals(differentRecipe));
-
     }
 
     @Test
-    void testRecipeSameName()
+    void testRecipeDiffCoffee()
     {
         recipe.setName("Lavazza");
         recipe.setAmtSugar("3");
@@ -165,5 +259,121 @@ class RecipeTest {
         sameRecipe.setAmtCoffee("3");
 
         assertFalse(recipe.equals(sameRecipe));
+    }
+
+    @Test
+    void testRecipeSameObj()
+    {
+        recipe.setName("Starbucks");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        assertTrue(recipe.equals(recipe));
+    }
+
+    @Test
+    void testRecipeDiffSugar()
+    {
+        recipe.setName("Lavazza");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        Recipe sameRecipe = new Recipe();
+
+        sameRecipe.setName("Lavazza");
+        sameRecipe.setAmtSugar("1");
+        sameRecipe.setPrice("4");
+        sameRecipe.setAmtMilk("2");
+        sameRecipe.setAmtChocolate("4");
+        sameRecipe.setAmtCoffee("2");
+
+        assertFalse(recipe.equals(sameRecipe));
+    }
+
+    @Test
+    void testRecipeDiffChocolate()
+    {
+        recipe.setName("Lavazza");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        Recipe sameRecipe = new Recipe();
+
+        sameRecipe.setName("Lavazza");
+        sameRecipe.setAmtSugar("3");
+        sameRecipe.setPrice("4");
+        sameRecipe.setAmtMilk("2");
+        sameRecipe.setAmtChocolate("1");
+        sameRecipe.setAmtCoffee("2");
+
+        assertFalse(recipe.equals(sameRecipe));
+    }
+
+    @Test
+    void testRecipeDiffMilk()
+    {
+        recipe.setName("Lavazza");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        Recipe sameRecipe = new Recipe();
+
+        sameRecipe.setName("Lavazza");
+        sameRecipe.setAmtSugar("3");
+        sameRecipe.setPrice("4");
+        sameRecipe.setAmtMilk("1");
+        sameRecipe.setAmtChocolate("4");
+        sameRecipe.setAmtCoffee("2");
+
+        assertFalse(recipe.equals(sameRecipe));
+    }
+
+    @Test
+    void testRecipeObject()
+    {
+        recipe.setName("Lavazza");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        Object sameRecipe = new Object();
+
+        assertFalse(recipe.equals(sameRecipe));
+    }
+
+    @Test
+    void testRecipeEqualNull()
+    {
+        recipe.setName("Lavazza");
+        recipe.setAmtSugar("3");
+        recipe.setPrice("4");
+        recipe.setAmtMilk("2");
+        recipe.setAmtChocolate("4");
+        recipe.setAmtCoffee("2");
+
+        Recipe diffRecipe = new Recipe();
+        diffRecipe.setAmtSugar("3");
+        diffRecipe.setPrice("4");
+        diffRecipe.setAmtMilk("2");
+        diffRecipe.setAmtChocolate("4");
+        diffRecipe.setAmtCoffee("2");
+        diffRecipe.setName("Lavazza");
+        diffRecipe.setName(null);
+
+        assertTrue(recipe.equals(diffRecipe));
     }
 }
